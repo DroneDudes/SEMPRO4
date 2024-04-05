0b509719b9f0e50d9acd2ab69aa89f90965387bc
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2024-04-05 09:18:53 +0200

    Added a base theme, and enabled HTTP requests in Angular

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            "primary": "#d700ff",
+                      
+            "secondary": "#dd8f00",
+                      
+            "accent": "#0084db",
+                      
+            "neutral": "#1b1b1b",
+                      
+            "base-100": "#ffffff",
+                      
+            "info": "#0064f5",
+                      
+            "success": "#43f76f",
+                      
+            "warning": "#e55000",
+                      
+            "error": "#c50027",
+          },
+        },
+      ],
+    },
   plugins: [require("daisyui")],
 }
 
