0b6069860a08f6cd9d178d0b6b261f9eb6087da3
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2024-04-07 11:21:02 +0200

    Revert "Added the baseline for AGV fundamentals"
    This reverts commit c7911f9ecaaca8cc4466c167bf26a05a241367f0.

--- backend/src/main/java/org/dronedudes/backend/BackendApplication.java
+++ backend/src/main/java/org/dronedudes/backend/BackendApplication.java
@@ -1,25 +1,13 @@
 package org.dronedudes.backend;
 
-import org.dronedudes.backend.agv.Agv;
-import org.dronedudes.backend.agv.AgvRepository;
-import org.dronedudes.backend.agv.AgvService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BackendApplication {
-	@Autowired
-	AgvRepository agvRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
