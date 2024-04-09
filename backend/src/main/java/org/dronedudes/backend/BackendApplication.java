4bcd5794f3428b82efbfd199c15ddd872fcecd71
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2024-04-09 10:53:03 +0200

    added application.properties to .gitignore
    and removed imports

--- backend/src/main/java/org/dronedudes/backend/BackendApplication.java
+++ backend/src/main/java/org/dronedudes/backend/BackendApplication.java
@@ -1,9 +1,5 @@
 package org.dronedudes.backend;
 
-import org.dronedudes.backend.agv.Agv;
-import org.dronedudes.backend.agv.AgvRepository;
-import org.dronedudes.backend.agv.AgvService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
