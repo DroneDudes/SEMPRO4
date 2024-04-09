c39a46b671d5299a8cda82b8f2359fb454c36caa
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2024-04-09 11:22:07 +0200

    V1 T1 Site Navigation (#1)
    * setup navbar - ready for review
    
    * navbar ready for review
    
    ---------
    
    Co-authored-by: Andreas Witten Honoré <honore.andreas@gmail.com>
    Co-authored-by: HenriikRC <112691706+HenriikRC@users.noreply.github.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {OverviewComponent} from "./overview/overview.component";
+import {ProductionComponent} from "./production/production.component";
+import {ManagementComponent} from "./management/management.component";
+import {BlueprintsComponent} from "./blueprints/blueprints.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: OverviewComponent},
+  {path: "production", component: ProductionComponent},
+  {path: "blueprints", component: BlueprintsComponent},
+  {path: "management", component: ManagementComponent},
+  {path: "**", component: OverviewComponent}
+];
