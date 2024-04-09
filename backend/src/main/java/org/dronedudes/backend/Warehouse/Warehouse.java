8d681669f80eab6fe59ebc09e1e318353ddc197c
deehstar <mortenegstrom@gmail.com>
2024-04-09 12:02:29 +0200

    Item array

--- backend/src/main/java/org/dronedudes/backend/Warehouse/Warehouse.java
+++ backend/src/main/java/org/dronedudes/backend/Warehouse/Warehouse.java
@@ -1,11 +1,12 @@
 package org.dronedudes.backend.Warehouse;
 
 import org.springframework.stereotype.Component;
-
+import org.dronedudes.backend.item.Item;
 @Component
 public class Warehouse {
     private int warehouseSize = 0;
-
+    private Item[] warehouseItems;
+    
     public void setWarehouseSize(int warehouseSize) {
         this.warehouseSize = warehouseSize;
     }
