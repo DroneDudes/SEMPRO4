616b9a197e69364309ad2c10eedbb00ca76f838e
deehstar <mortenegstrom@gmail.com>
2024-04-09 11:46:04 +0200

    warehouseSize
    warehouseSize attribute created
    getter and setter

--- backend/src/main/java/org/dronedudes/backend/Warehouse/Warehouse.java
+++ backend/src/main/java/org/dronedudes/backend/Warehouse/Warehouse.java
@@ -4,5 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Warehouse {
-    
+    private int warehouseSize = 0;
+
+    public void setWarehouseSize(int warehouseSize) {
+        this.warehouseSize = warehouseSize;
+    }
+    public int getSize() {
+        return warehouseSize;
+    }
 }
