3b1871987283bcd49ced834177b499a386a4afc5
msundby <114078619+msundby@users.noreply.github.com>
2024-04-10 14:08:34 +0200

    Service, controller and repository set up for Parts. Create parts implemented.

--- backend/src/main/java/org/dronedudes/backend/Part/Part.java
+++ backend/src/main/java/org/dronedudes/backend/Part/Part.java
@@ -1,7 +1,23 @@
 package org.dronedudes.backend.Part;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Part {
 
-    String partTitle;
-    String description;
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private long id;
+   @Column(unique = true)
+   private String name;
+   private String description;
+   private String specifications;
+   private String supplierDetails;
+   private long price;
 }
---
+++ backend/src/main/java/org/dronedudes/backend/Part/PartService.java
@@ -0,0 +1,26 @@
+package org.dronedudes.backend.Part;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PartService {
+
+    private final PartRepository partRepository;
+
+    @Autowired
+    public PartService(PartRepository partRepository) {
+        this.partRepository = partRepository;
+    }
+
+    public Part createPart(Part part){
+        partRepository.save(part);
+        return part;
+    }
+
+    public List<Part> findAllParts() {
+        return partRepository.findAll();
+    }
+}
