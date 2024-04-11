f3083f35fdaf21c1bcc5d9dfedb4986c8e03dba9
deehstar <mortenegstrom@gmail.com>
2024-04-11 12:19:51 +0200

    Warehouse Repository implemented

---
+++ backend/src/main/java/org/dronedudes/backend/Warehouse/WarehouseRepository.java
@@ -0,0 +1,9 @@
+package org.dronedudes.backend.Warehouse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WarehouseRepository extends JpaRepository<Long, Warehouse> {
+        
+}
