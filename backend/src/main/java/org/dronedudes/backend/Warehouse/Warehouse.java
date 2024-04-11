4dec9fbe1536d854a806c95268061609e850258b
deehstar <mortenegstrom@gmail.com>
2024-04-11 11:58:42 +0200

    Warehouse Entity done

--- backend/src/main/java/org/dronedudes/backend/Warehouse/Warehouse.java
+++ backend/src/main/java/org/dronedudes/backend/Warehouse/Warehouse.java
@@ -1,36 +1,30 @@
 package org.dronedudes.backend.Warehouse;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.dronedudes.backend.item.Item;
 import org.hibernate.annotations.ManyToAny;
 
-import java.util.Map;
-import java.util.Set;
-
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Warehouse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private WarehouseModel model;
     @ManyToAny
-    private Set<Item> items;
-    private String uri = "http://localhost:8081/Service.asmx";
-    private WarehouseCommunicationProtocol warehouseCommunicationProtocol;
+    private Item[] items;
+    private String uri;
+    private String name;
 
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-
-    public WarehouseCommunicationProtocol getWarehouseCommunicationProtocol() {
-        return warehouseCommunicationProtocol;
-    }
-
-    public void setWarehouseCommunicationProtocol(WarehouseCommunicationProtocol warehouseCommunicationProtocol) {
-        this.warehouseCommunicationProtocol = warehouseCommunicationProtocol;
+    public Warehouse(WarehouseModel model, int port, String name) {
+        this.model = model;
+        this.items = new Item[model.getSize()];
+        this.uri = model.getBaseUri() + port + model.getSuffixUri();
+        this.name = name;
     }
 }
