8e2b687655a1d99d57f1fd8aae856ea2cc60a069
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2024-04-25 12:50:16 +0200

    Created the production batch overview

--- frontend/src/app/production/production.component.ts
+++ frontend/src/app/production/production.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface ProductionBatch {
+  batchNumber: number;
+  startTime: string;
+  elapsedTime: string;
+  completionTime: string;
+  employeeId: number;
+}
+
 @Component({
   selector: 'app-production',
   standalone: true,
@@ -8,5 +16,8 @@ import { Component } from '@angular/core';
   styleUrl: './production.component.css'
 })
 export class ProductionComponent {
-
+  public tableHeaders: string[] = ["Batch number", "Start time", "Elapsed time", "Completion time", "Employee id"];
+  public productionBatches: ProductionBatch[] = [
+    { batchNumber: 1, startTime: "2021-01-01 12:00:00", elapsedTime: "00:00:00", completionTime: "2021-01-01 13:00:00", employeeId: 1 }
+  ];
 }
