63d39f731003bc6a85721d91841d948f78ecab9e
HenriikRC <hechr16@student.sdu.dk>
2024-05-27 09:03:24 +0200

    startProduction

--- frontend/src/app/production/production.component.ts
+++ frontend/src/app/production/production.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductionBatchComponent } from './production-batch/production-batch.component';
+import { ProductionService } from './_services/production.service';
 
 @Component({
   selector: 'app-production',
   standalone: true,
   imports: [ProductionBatchComponent],
   templateUrl: './production.component.html',
-  styleUrl: './production.component.css'
+  styleUrls: ['./production.component.css']
 })
-export class ProductionComponent {
+export class ProductionComponent implements OnInit {
+  blueprints: any[] = [];
+  selectedBlueprint: any = null;
+  productionAmount: number = 1;
+  submitted: boolean = false;
 
+  constructor(private productionService: ProductionService) { }
+
+  ngOnInit(): void {
+    this.fetchBlueprints();
+  }
+
+  fetchBlueprints(): void {
+    this.productionService.getAllBlueprints().subscribe(
+      data => {
+        this.blueprints = data;
+        console.log("Fetched Blueprints:", this.blueprints);
+      },
+      error => {
+        console.error("Error fetching blueprints:", error);
+      }
+    );
+  }
+
+  setSelectedBlueprint(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedIndex = selectElement.selectedIndex;
+    if (selectedIndex >= 0) {
+      this.selectedBlueprint = this.blueprints[selectedIndex];
+      console.log("Selected Blueprint:", this.selectedBlueprint);
+    }
+  }
+
+  setProductionAmount(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.productionAmount = parseInt(inputElement.value, 10);
+  }
+
+  startProduction(): void {
+    this.submitted = true;
+    console.log("Selected Blueprint:", this.selectedBlueprint);
+    console.log("Production Amount:", this.productionAmount);
+    if (this.selectedBlueprint && this.productionAmount > 0) {
+      this.productionService.startProduction(this.productionAmount, this.selectedBlueprint).subscribe(response => {
+        console.log("Response from backend:", response);
+        alert(response);
+      }, error => {
+        console.error("Error from backend:", error);
+      });
+    }
+  }
 }
